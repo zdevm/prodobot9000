b2e4c8ea3295c78a757edc029235a6e64d705a24
Marios Zafeiriou <zafeidev@gmail.com>
2022-08-14 15:31:34 +0300

    refactor jwt guard

--- src/modules/auth/guards/jwt.guard.ts
+++ src/modules/auth/guards/jwt.guard.ts
@@ -1,6 +1,13 @@
 import { JwtStrategyName } from '@modules/auth/strategies/jwt.strategy';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard(JwtStrategyName) {}
+export class JwtAuthGuard extends AuthGuard(JwtStrategyName) {
+  handleRequest(err: any, user: any, info: any) {
+    if (err || !user) {
+      throw err || new UnauthorizedException('INVALID_ACCESS_TOKEN');
+    }
+    return user;
+  }
+}
